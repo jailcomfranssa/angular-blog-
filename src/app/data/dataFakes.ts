ef6313b10d02f364ea49882f447a609068d0a543
jailcomfranssa <jail_son_0@hotmail.com>
2023-03-13 16:39:02 -0300

    deixando o content page dinamico.

--- src/app/data/dataFakes.ts
+++ src/app/data/dataFakes.ts
@@ -2,13 +2,32 @@ export const dataFakes =[
   {
     "id":1,
     "photo":"aaa",
-    "title": "novo",
-    "description":"ola"
+    "title": "Angular Software",
+    "description":"Angular é uma plataforma de aplicações web de código-fonte aberto\n" +
+      "e front-end baseado em TypeScript liderado pela Equipe Angular do Google e por uma comunidade de indivíduos e corporações.",
+    "photoCover":"https://www.zup.com.br/wp-content/uploads/2021/03/5e32dc5538c14b0d4b28f87f_imagens-angular3.png"
   },
   {
     "id":2,
     "photo":"aaa",
-    "title": "novo",
-    "description":"ola"
+    "title": "Angular: tudo sobre versões e releases",
+    "description":"ola",
+    "photoCover":"https://blog.geekhunter.com.br/wp-content/uploads/2019/10/Um-overview-sobre-o-framework-Angular.jpg"
+  },
+
+  {
+    "id":3,
+    "photo":"aaa",
+    "title": "Top 18 Most Common Mistakes that AngularJS Developers Make",
+    "description":"AngularJS is a very powerful framework. It is the third most starred repository on GitHub. It is not difficult to start using, but the goals that it is intended to accomplish demand comprehension.",
+    "photoCover":"https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/1096616/retina_1708x683_cover-top-18-most-common-angularjs-developer-mistakes-41f9ad303a51db70e4a5204e101e7414.png"
+  },
+
+  {
+    "id":4,
+    "photo":"aaa",
+    "title": "Introduction to Angular Features",
+    "description":"Features of Angular comes into the picture for front-end development of the application. In the minimum time, it becomes very popular and it is the best single page web application development solution.",
+    "photoCover":"https://cdn.educba.com/academy/wp-content/uploads/2020/02/angular-features.jpg"
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {dataFakes} from '../../data/dataFakes';
 
 @Component({
   selector: 'app-content',
@@ -11,13 +12,24 @@ export class ContentComponent implements OnInit {
   photoCover:string = "https://www.agiledrop.com/sites/default/files/styles/blog_article/public/2020-03/Angular%20or%20AngularJS%20cover%20photo.png?itok=e-xGVeU-"
   contentTitle:string = "Noticias"
   contentDescription:string = "Ola Mundo!"
+  private Id:string | null = "0"
 
   constructor( private route:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.Id = value.get("id")
     )
+    this.setValuesToComponent(this.Id)
+  }
+
+  setValuesToComponent(id: string | null){
+    const result = dataFakes.filter(article=> article.id.toString() == id)[0]
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+
+
   }
 
 }
